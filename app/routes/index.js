c2260803166316208a883fb3bd24ca7eb1843e9d
cory <whippoorwill.in.flight@gmail.com>
2016-06-16 14:21:49 -0700

    start question-tile

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,7 +3,10 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return this.store.findAll('answer');
+    return Ember.RSVP.hash({
+      questions: this.store.findAll('question'),
+      answers: this.store.findAll('answer')
+    });
   },
 
   actions: {
